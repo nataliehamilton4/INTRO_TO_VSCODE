c09dd7a943555a9f9ccd02bf0118c2ae29710b77
Natalie Hamilton <natalie_hamilton4@baylor.edu>
2024-09-03 15:40:18 -0500

    added new print statement

---
+++ mygraph.py
@@ -0,0 +1,21 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linespace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+
+# plt.show()
+
+#problem with line 1, library is not installed
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+
+plt.show()
+
+print("Hello there!")
+
+print("Hellow again!")
---
+++ python1.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c #the debugger stops before the line that it is on (debugger starts prior to)
+
+
+message = "Hello World!"
+
+print(message)
+
+print()
